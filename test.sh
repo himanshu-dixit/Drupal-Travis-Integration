0b284fa3447779b92e6d4de643088a0e2b1adc73
Himanshu Dixit <hudixt@gmail.com>
2017-10-26 18:48:24 +0530

    test

--- test.sh
+++ test.sh
@@ -1,5 +1,2 @@
-cd core
-../vendor/bin/phpunit --testsuite=unit
-
-cd ../
+vendor/bin/phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer
 vendor/bin/phpcs -n --standard=Drupal modules/*
