da0dccc1332113e39b032ac761819cfa3effc55a
Himanshu Dixit <hudixt@gmail.com>
2017-10-26 18:58:37 +0530

    Finalize

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
+echo "Running PHPCS"
 vendor/bin/phpcs --config-set installed_paths ~/.composer/vendor/drupal/coder/coder_sniffer
 vendor/bin/phpcs -n --standard=Drupal modules/*
 
+echo "Running Drupal Unit Tests"
 cd core
 ../vendor/bin/phpunit --testsuite=unit
